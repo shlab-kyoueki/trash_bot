f3e6e563fb2668416ff17a580944039d3f1c8cc1
Glitch (greet-bot) <none>
2024-02-29 13:11:36 +0000

    🐪🙉 Checkpoint ./index.js:65509748/1924

--- index.js
+++ index.js
@@ -6,28 +6,43 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
-const b4310 = ["あああ", "いいい", "ううう", "えええ", "おおお"];
-const m1310 = ["かかか", "ききき", "くくく", "けけけ", "こここ"];
+const b4306 = ["あああ", "いいい", "ううう", "えええ", "おおお"];
+const m1306 = ["かかか", "ききき", "くくく", "けけけ", "こここ"];
+const b4310 = ["さささ", "ししし", "すすす"];
+const m1310 = ["たたた", "ちちち", "つつつ", "ててて"];
+const b4315 = ["ななな", "ににに"];
+const m1315 = ["ははは", "ひひひ", "ふふふ", "へへへ", "ほほほ"];
 
 //メッセージが投稿された時に呼ばれるメソッド
 app.message(async ({ message, say }) => {
   var b4;
   var m1;
+  const numberRegex = /\d+/g;
   //依頼判定
   if (message.text.includes("abc")) {
-    await say(message.text);
-    const numbers = message.match(/\d+/g); //部屋の取得
+    const numbers = message.text.match(numberRegex); //部屋の取得
     if (numbers) {
-      const result = numbers.join(", ");
+      const room = numbers.join(", ");
+      if (room == 306) {
+        var randomIndex = Math.floor(Math.random() * b4306.length);
+        b4 = b4306[randomIndex];
+        randomIndex = Math.floor(Math.random() * m1306.length);
+        m1 = m1306[randomIndex];
+      }
+      else if (room == 310) {
+        var randomIndex = Math.floor(Math.random() * b4310.length);
+        b4 = b4310[randomIndex];
+        randomIndex = Math.floor(Math.random() * m1310.length);
+        m1 = m1310[randomIndex];
+      }
+      else if (room == 315) {
+        var randomIndex = Math.floor(Math.random() * b4315.length);
+        b4 = b4315[randomIndex];
+        randomIndex = Math.floor(Math.random() * m1315.length);
+        m1 = m1315[randomIndex];
+      }
+      await say(`${b4}, ${m1}`);
     }
-    await say(room);
-    if (room == 310) {
-      var randomIndex = Math.floor(Math.random() * b4310.length);
-      b4 = b4310[randomIndex];
-      randomIndex = Math.floor(Math.random() * m1310.length);
-      m1 = m1310[randomIndex];
-    }
-    await say(`Hey there <@${message.user}>!`);
   }
 });
 //アプリが起動時に呼ばれるメソッド
