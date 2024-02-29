8151ba22446a3cfdb3a1a5e1010dc3f8c4d576f3
Glitch (greet-bot) <none>
2024-02-29 15:12:57 +0000

    👔🌓 Checkpoint ./index.js:65511679/1319

--- index.js
+++ index.js
@@ -29,23 +29,48 @@ app.message(async ({ message, say }) => {
       const room = numbers.join(", ");
       //b4,m1選定（ランダムに取得)
       if (room == 306) {
-        var randomIndex = Math.floor(Math.random() * b4306.length);
-        b4 = b4306[randomIndex];
-        randomIndex = Math.floor(Math.random() * m1306.length);
-        m1 = m1306[randomIndex];
+        var randomIndex = Math.floor(Math.random() * arrayFromFile.b4306.length);
+        b4 = arrayFromFile.b4306[randomIndex];
+        arrayFromFile.b4306.splice(randomIndex, 1);
+        if(arrayFromFile.b4306.length==0){
+          arrayFromFile.b4306=b4306;
+        }
+        randomIndex = Math.floor(Math.random() * arrayFromFile.m1306.length);
+        m1 = arrayFromFile.m1306[randomIndex];
+        arrayFromFile.m1306.splice(randomIndex, 1);
+        if(arrayFromFile.m1306.length==0){
+          arrayFromFile.m1306=m1306;
+        }
       }
       else if (room == 310) {
-        var randomIndex = Math.floor(Math.random() * b4310.length);
-        b4 = b4310[randomIndex];
-        randomIndex = Math.floor(Math.random() * m1310.length);
-        m1 = m1310[randomIndex];
+        var randomIndex = Math.floor(Math.random() * arrayFromFile.b4310.length);
+        b4 = arrayFromFile.b4310[randomIndex];
+        arrayFromFile.b4310.splice(randomIndex, 1);
+        if(arrayFromFile.b4310.length==0){
+          arrayFromFile.b4310=b4310;
+        }
+        randomIndex = Math.floor(Math.random() * arrayFromFile.m1310.length);
+        m1 = arrayFromFile.m1310[randomIndex];
+        arrayFromFile.m1310.splice(randomIndex, 1);
+        if(arrayFromFile.m1310.length==0){
+          arrayFromFile.m1310=m1310;
+        }
       }
       else if (room == 315) {
-        var randomIndex = Math.floor(Math.random() * b4315.length);
-        b4 = b4315[randomIndex];
-        randomIndex = Math.floor(Math.random() * m1315.length);
-        m1 = m1315[randomIndex];
+        var randomIndex = Math.floor(Math.random() * arrayFromFile.b4315.length);
+        b4 = arrayFromFile.b4315[randomIndex];
+        arrayFromFile.b4315.splice(randomIndex, 1);
+        if(arrayFromFile.b4315.length==0){
+          arrayFromFile.b4315=b4315;
+        }
+        randomIndex = Math.floor(Math.random() * arrayFromFile.m1315.length);
+        m1 = arrayFromFile.m1315[randomIndex];
+        arrayFromFile.m1315.splice(randomIndex, 1);
+        if(arrayFromFile.m1315.length==0){
+          arrayFromFile.m1315=m1315;
+        }
       }
+      fs.writeFileSync(filePath, JSON.stringify(arrayFromFile));
       await say(`${b4}, ${m1}`);
     }
   }
