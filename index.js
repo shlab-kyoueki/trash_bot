5ed44e3278ffe1cddd8a3f01e39e13810e264ec9
Glitch (greet-bot) <none>
2024-04-09 05:03:58 +0000

    😽🚡 Checkpoint ./index.js:65511679/794

--- index.js
+++ index.js
@@ -12,33 +12,35 @@ const app = new App({
 // const m1310 = ["たたた", "ちちち", "つつつ", "ててて"];
 // const b4315 = ["ななな", "ににに"];
 // const m1315 = ["ははは", "ひひひ", "ふふふ", "へへへ", "ほほほ"];
+const b4_def=["上池 陽汰","久野 翔也","住本 圭","関 隆斗", "中島 魁人", "大野 伶将", "豊島 光太", "前田 篤志", "森中 誠也", "山下 理子", "松山 僚太"];
+const m1_def=["宇都宮 優巳","王 若泰","川上 拓真","永井 寿弥", "名執 凌磨", "定 良祐", "前河 有紀", "森田 光陽", "安田 隆一"];
 
 //メッセージが投稿された時に呼ばれるメソッド
 app.message(async ({ message, say }) => {
   const fs = require('fs');
-  const filePath = 'arrayData.json';
+  const filePath = 'arrayData_rev.json';
   const data = fs.readFileSync(filePath, 'utf8');
   const arrayFromFile = JSON.parse(data);
   var b4;
   var m1;
   const numberRegex = /\d+/g;
   //依頼判定
-  if (message.text.includes("abc")) {
+  if (message.text.includes("ゴミ出し")) {
     // const numbers = message.text.match(numberRegex); //部屋の取得
     //b4,m1選定（ランダムに取得)
     // if (numbers) {
       // const room = numbers.join(", ");
-      var randomIndex = Math.floor(Math.random() * arrayFromFile.b4306.length);
-        b4 = arrayFromFile.b4306[randomIndex];
-        arrayFromFile.b4306.splice(randomIndex, 1);
-        if(arrayFromFile.b4306.length==0){
-          arrayFromFile.b4306=b4306;
+      var randomIndex = Math.floor(Math.random() * arrayFromFile.b4.length);
+        b4 = arrayFromFile.b4[randomIndex];
+        arrayFromFile.b4.splice(randomIndex, 1);
+        if(arrayFromFile.b4.length==0){
+          arrayFromFile.b4=b4;
         }
-        randomIndex = Math.floor(Math.random() * arrayFromFile.m1306.length);
-        m1 = arrayFromFile.m1306[randomIndex];
-        arrayFromFile.m1306.splice(randomIndex, 1);
-        if(arrayFromFile.m1306.length==0){
-          arrayFromFile.m1306=m1306;
+        randomIndex = Math.floor(Math.random() * arrayFromFile.m1.length);
+        m1 = arrayFromFile.m1[randomIndex];
+        arrayFromFile.m1.splice(randomIndex, 1);
+        if(arrayFromFile.m1.length==0){
+          arrayFromFile.m1=m1;
         }
       // if (room == 306) {
       //   var randomIndex = Math.floor(Math.random() * arrayFromFile.b4306.length);
@@ -84,15 +86,20 @@ app.message(async ({ message, say }) => {
       // }
       fs.writeFileSync(filePath, JSON.stringify(arrayFromFile));
       await say(`${b4}, ${m1}`);
-    }
+    // }
   }
+  // else if(message.text.includes("reset")){
+  //   arrayFromFile.b4306=b4306;
+  //   arrayFromFile.m1306=m1306;
+  //   arrayFromFile.b4310=b4310;
+  //   arrayFromFile.m1310=m1310;
+  //   arrayFromFile.b4315=b4315;
+  //   arrayFromFile.m1315=m1315;
+  //   await say(`ok`);
+  // }
   else if(message.text.includes("reset")){
-    arrayFromFile.b4306=b4306;
-    arrayFromFile.m1306=m1306;
-    arrayFromFile.b4310=b4310;
-    arrayFromFile.m1310=m1310;
-    arrayFromFile.b4315=b4315;
-    arrayFromFile.m1315=m1315;
+    arrayFromFile.b4=b4;
+    arrayFromFile.m1=m1;
     await say(`ok`);
   }
 });
