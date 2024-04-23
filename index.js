f60d7ed45fd358ba95e43aac7612d4df06c3c360
Glitch (greet-bot) <none>
2024-04-23 10:58:18 +0000

    👹🌃 Checkpoint ./index.js:65511679/314

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+//実際に指名するのはjsonファイルから選ぶ
 const b4_def=["上池 陽汰","久野 翔也","住本 圭","関 隆斗", "中島 魁人", "大野 伶将", "豊島 光太", "前田 篤志", "森中 誠也", "山下 理子"];
 const m1_def=["宇都宮 優巳","王 若泰","川上 拓真","永井 寿弥", "名執 凌磨", "定 良祐", "前河 有紀", "森田 光陽", "安田 隆一", "松山 僚太", "Cai Beicinger", "Wang Yuhao"];
 
@@ -21,15 +22,15 @@ app.message(async ({ message, say }) => {
   //依頼判定
   if (message.text.includes("ゴミ出しお願いします")) {
       randomIndex = Math.floor(Math.random() * arrayFromFile.m1.length);
-      m1 = arrayFromFile.m1[randomIndex];
-      arrayFromFile.m1.splice(randomIndex, 1);
-      if(arrayFromFile.m1.length==0){
+      m1 = arrayFromFile.m1[randomIndex];  //指名する人を求める
+      arrayFromFile.m1.splice(randomIndex, 1);  //指名した分削る
+      if(arrayFromFile.m1.length==0){  //いなくなったら補充
         arrayFromFile.m1=m1_def;
       }
       var randomIndex = Math.floor(Math.random() * arrayFromFile.b4.length);
-      b4 = arrayFromFile.b4[randomIndex];
-      arrayFromFile.b4.splice(randomIndex, 1);
-      if(arrayFromFile.b4.length==0){
+      b4 = arrayFromFile.b4[randomIndex];  //指名する人を求める
+      arrayFromFile.b4.splice(randomIndex, 1);  //指名した分削る
+      if(arrayFromFile.b4.length==0){  //いなくなったら補充
         arrayFromFile.b4=b4_def;
       }
       fs.writeFileSync(filePath, JSON.stringify(arrayFromFile));
